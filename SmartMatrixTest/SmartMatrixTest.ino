7a37808f578be099062915cccae310dd18099524
Isdogmc <100045710+Isdogmc@users.noreply.github.com>
2023-02-21 21:11:17 -0600

    Update SmartMatrixTest.ino

--- SmartMatrixTest/SmartMatrixTest.ino
+++ SmartMatrixTest/SmartMatrixTest.ino
@@ -6,7 +6,7 @@
 
 #define COLOR_DEPTH 24
 const uint16_t kMatrixWidth = 32;       // Set to the width of your display, must be a multiple of 8
-const uint16_t kMatrixHeight = 32;      // Set to the height of your display
+const uint16_t kMatrixHeight = 16;      // Set to the height of your display
 const uint8_t kRefreshDepth = 36;       // Tradeoff of color quality vs refresh rate, max brightness, and RAM usage.  36 is typically good, drop down to 24 if you need to.  On Teensy, multiples of 3, up to 48: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48.  On ESP32: 24, 36, 48
 const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save RAM, more to keep from dropping frames and automatically lowering refresh rate.  (This isn't used on ESP32, leave as default)
 const uint8_t kPanelType = SM_PANELTYPE_HUB75_32ROW_MOD16SCAN;   // Choose the configuration that matches your panels.  See more details in MatrixCommonHub75.h and the docs: https://github.com/pixelmatix/SmartMatrix/wiki
@@ -40,17 +40,18 @@ void setup() {
   
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
+
 }
 
 void loop() {
- rgb24 *buffer = backgroundLayer.backBuffer();
+rgb24 *buffer = backgroundLayer.backBuffer();
   
-setcolor(2, 2, (rgb24)CRGB(0,0,0));
+setcolor(5, 2, (rgb24)CRGB::Black);
 backgroundLayer.swapBuffers(false);
 delay(1000);
 
 
-setcolor(2, 2, (rgb24)CRGB(0,255,0));
+setcolor(5, 2, (rgb24)CRGB::Red);
 backgroundLayer.swapBuffers(false);
 delay(1000);
 
@@ -59,13 +60,13 @@ delay(1000);
 }
 
 void setcolor(int row , int col, rgb24 color) { //setcolor(2, 2, (rgb24)CRGB(0,255,0));
-  rgb24 *buffer = backgroundLayer.backBuffer();
+  rgb24 *buffer = backgroundLayer.backBuffer(); //setcolor(2, 2, (rgb24)CRGB::Color);
   int n = 0;
-  if (row % 2 == 0) {
-    n = row * WIDTH + col;
-  } else {
-    n = row * WIDTH + (WIDTH - col - 1);
-  }
+  n = row * WIDTH + col;
   buffer[n] = color;
 }
+
+
+
+
 
